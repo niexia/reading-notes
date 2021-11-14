4b1d43273593eb5c756677941b2cfbeb872c34fe
杨金 <yangjin.im@bytedance.com>
2021-11-14 18:46:20 +0800

    feat: 增加硅谷钢铁侠的笔记

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -48,6 +48,13 @@ module.exports = config({
                 link: '/the-clean-coder/',
               },
             ]
+          },
+          {
+            text: '人物传记',
+            items: [{
+              text: '硅谷钢铁侠',
+              link: '/elon-musk/',
+            }, ]
           }
         ]
       },
@@ -60,6 +67,7 @@ module.exports = config({
     sidebar: {
       '/learn-architecture-from-zero/': getLearnArchitectureFromZeroSideBar(),
       '/the-clean-coder/': getTheCleanCoderSideBar(),
+      '/elon-musk/': getTheElonMuskBiographySideBar(),
     }
   },
   plugins: [
@@ -138,6 +146,88 @@ function getTheCleanCoderSideBar() {
     sidebarDepth: 2,
     children: [
       'say-no/say-no',
+    ]
+  }, ]
+}
+
+function getTheElonMuskBiographySideBar() {
+  return [{
+    title: '马斯克的世界：跨领域创造',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      ['', '硅谷钢铁侠'],
+      'musk-world/',
+    ]
+  }, {
+    title: '出生非洲地：冒险无极限的基因',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'born-in-africa/',
+    ]
+  }, {
+    title: '挺进加拿大：追寻太阳的人',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'advance-to-canada/',
+    ]
+  }, {
+    title: '第一次创业：征服网络世界',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'first-business/',
+    ]
+  }, {
+    title: 'PayPal 黑帮大佬：发动国际金融革命',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'paypal-boss/',
+    ]
+  }, {
+    title: '太空召唤：建立 SpaceX 创新大军',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'call-of-space/',
+    ]
+  }, {
+    title: '全电动车：超酷超快的特斯拉',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'all-electric-car/',
+    ]
+  }, {
+    title: '痛苦磨难与新生：现实版钢铁侠的商业版图',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'pain-and-rebirth/',
+    ]
+  }, {
+    title: '腾飞：被颠覆的航空业',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'take-off/',
+    ]
+  }, {
+    title: '电动车的复仇：毫不妥协换来最好的时机',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'revenge-of-the-electric-car/',
+    ]
+  }, {
+    title: '埃隆.马斯克的统一场理论：下一个 10 年',
+    collapsable: false,
+    sidebarDepth: 2,
+    children: [
+      'elon-musk-unified-field-theory/',
     ]
   }, ]
 }
