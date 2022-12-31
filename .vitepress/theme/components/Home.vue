40cdeaf2ff7359f005a478ca0f0cf66441fddfb3
yangjin <yangjin.im@bytedance.com>
2022-12-31 20:20:39 +0800

    chore: 切换成 vitepress vue 主题

---
+++ .vitepress/theme/components/Home.vue
@@ -0,0 +1,203 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section id="hero">
+    <h1 class="tagline">
+      广泛
+      <span class="accent">阅读</span>
+      <br />尝试新事物
+    </h1>
+    <p class="description">
+      一本个人的, 种类多的, 持续更新的, 认真整理的阅读笔记
+    </p>
+    <p class="actions">
+      <a class="get-started" href="/guide/introduction.html">
+        Get Started
+        <svg
+          class="icon"
+          xmlns="http://www.w3.org/2000/svg"
+          width="10"
+          height="10"
+          viewBox="0 0 24 24"
+        >
+          <path
+            d="M13.025 1l-2.847 2.828 6.176 6.176h-16.354v3.992h16.354l-6.176 6.176 2.847 2.828 10.975-11z"
+          />
+        </svg>
+      </a>
+    </p>
+  </section>
+
+  <section id="highlights" class="vt-box-container">
+    <div class="vt-box">
+      <h2>种类多</h2>
+      <p>
+        根据每年的安排，有计划没计划的确定一些数据进行阅读，包含不同类型的书籍
+      </p>
+    </div>
+    <div class="vt-box">
+      <h2>持续更新</h2>
+      <p>
+        保持阅读习惯，并且可以定期更新文档，通过阅读不断的积累和沉淀
+      </p>
+    </div>
+    <div class="vt-box">
+      <h2>认真整理</h2>
+      <p>
+        所有笔记都认真整理，能够对文档进行很好的总结归纳，方便再次快速查看和记忆
+      </p>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+section {
+  padding: 42px 32px;
+}
+
+#hero {
+  padding: 96px 32px;
+  text-align: center;
+}
+
+.tagline {
+  font-size: 76px;
+  line-height: 1.25;
+  font-weight: 900;
+  letter-spacing: -1.5px;
+  max-width: 960px;
+  margin: 0px auto;
+}
+
+html:not(.dark) .accent,
+.dark .tagline {
+  background: -webkit-linear-gradient(315deg, #42d392 25%, #647eff);
+  background-clip: text;
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+
+.description {
+  max-width: 960px;
+  line-height: 1.5;
+  color: var(--vt-c-text-2);
+  transition: color 0.5s;
+  font-size: 22px;
+  margin: 24px auto 40px;
+}
+
+.actions a {
+  font-size: 16px;
+  display: inline-block;
+  background-color: var(--vt-c-bg-mute);
+  padding: 8px 18px;
+  font-weight: 500;
+  border-radius: 8px;
+  transition: background-color 0.5s, color 0.5s;
+}
+
+.actions .get-started {
+  margin-right: 18px;
+}
+
+.actions .icon {
+  display: inline;
+  position: relative;
+  top: -1px;
+  margin-left: 2px;
+  fill: currentColor;
+  transition: transform 0.2s;
+}
+
+.actions .get-started:hover {
+  transition-duration: 0.2s;
+}
+
+.actions .get-started:hover .icon {
+  transform: translateX(2px);
+}
+
+.actions .get-started,
+.actions .setup {
+  color: var(--vt-c-text-code);
+}
+
+.actions .get-started:hover,
+.actions .setup:hover {
+  background-color: var(--vt-c-gray-light-4);
+  transition-duration: 0.2s;
+}
+
+.dark .actions .get-started:hover,
+.dark .actions .setup:hover {
+  background-color: var(--vt-c-gray-dark-3);
+}
+
+#highlights {
+  max-width: 960px;
+  margin: 0px auto;
+  color: var(--vt-c-text-2);
+}
+
+#highlights h2 {
+  font-weight: 600;
+  font-size: 20px;
+  letter-spacing: -0.4px;
+  color: var(--vt-c-text-1);
+  transition: color 0.5s;
+  margin-bottom: 0.75em;
+}
+
+#highlights p {
+  font-weight: 400;
+  font-size: 15px;
+}
+
+#highlights .vt-box {
+  background-color: transparent;
+}
+
+@media (max-width: 960px) {
+  .tagline {
+    font-size: 64px;
+    letter-spacing: -0.5px;
+  }
+  .description {
+    font-size: 18px;
+    margin-bottom: 48px;
+  }
+}
+
+@media (max-width: 768px) {
+  .tagline {
+    font-size: 48px;
+    letter-spacing: -0.5px;
+  }
+}
+
+@media (max-width: 576px) {
+  #hero {
+    padding: 64px 32px;
+  }
+  .description {
+    font-size: 16px;
+    margin: 18px 0 30px;
+  }
+  #highlights h3 {
+    margin-bottom: 0.6em;
+  }
+  #highlights .vt-box {
+    padding: 20px 36px;
+  }
+  .actions a {
+    margin: 0.5em 0;
+  }
+}
+
+@media (max-width: 370px) {
+  .tagline {
+    font-size: 36px;
+  }
+}
+</style>
---
+++ src/index.md
@@ -0,0 +1,10 @@
+---
+page: true
+title: Reading note
+---
+
+<script setup>
+import Home from '@theme/components/Home.vue'
+</script>
+
+<Home />
